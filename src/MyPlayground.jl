249067103150f3de4b50f7bc3b2f1a08692283b7
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-20 17:55:37 -0700

    Add some deprecated syntax

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -1,5 +1,16 @@
 module MyPlayground
 
-# package code goes here
+immutable BadStruct{T}
+    x::T
+end
+
+bad_identity{T}(x::T) = x
+
+typealias BadVec{T} Array{T, 1}
+
+function bad_underscore(x)
+    _ = x
+    return _
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,4 @@ else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+@test_approx_eq 1 1
