1ab3b6dac5887f2c4c3ed1d099410e32f22e27e1
Takafumi Arakaki <aka.tkf@gmail.com>
2018-11-01 23:29:41 -0700

    Check reinterpret

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,13 @@ using MyPlayground
 else
     using Test
 end
+
+@testset for src in [Int32[1], Int32[65536], Int32[1, 2], [1, 2]]
+    dest = copy(reinterpret(UInt8, src))
+    fill!(dest, 0)
+    dest_rint = reinterpret(eltype(src), dest)
+    copyto!(dest_rint, src)
+    @show dest
+    @show dest_rint
+    @test dest_rint == src
+end
