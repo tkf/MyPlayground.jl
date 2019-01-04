8eed2b16e202fa52387d28dcf2958be020bd9a03
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-03 17:33:17 -0800

    Add a private function

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -9,4 +9,11 @@ This function is exported.
 """
 my_public_function() = nothing
 
+"""
+    my_private_function()
+
+This function is _not_ exported.
+"""
+my_private_function() = nothing
+
 end # module
