b571d56e8fcb1d1e00c53cf2b4b95ff5c2068892
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-28 16:02:02 -0700

    Alter `Base.LOAD_PATH` rather than `Pkg.develop`

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -7,12 +7,8 @@ const gluepkg = Base.PkgId(Base.UUID("197e495a-9878-11e9-311c-51fb68a00c9c"),
                            "GlueModule")
 
 function __init__()
+    pushfirst!(Base.LOAD_PATH, dirname(@__DIR__))
     @require JSON="682c06a0-de6a-54ab-a142-c8b1cf79cde6" begin
-        if Base.locate_package(gluepkg) === nothing
-            Pkg.develop(PackageSpec(
-                path=joinpath(@__DIR__, "..", "GlueModule")
-            ))
-        end
         const GlueModule = Base.require(gluepkg)
     end
 end
