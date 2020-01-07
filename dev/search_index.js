c93053f3d6346a5de4f54f0937a28d27ec6bf5a4
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-07 03:30:15 +0000

    build based on b152d62

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyPlayground.jl-1","page":"MyPlayground.jl","title":"MyPlayground.jl","text":"","category":"section"},{"location":"#","page":"MyPlayground.jl","title":"MyPlayground.jl","text":"Documentation for MyPlayground.jl","category":"page"}]
+}
