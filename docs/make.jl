b0d69661fbd265ecc3d53a11e8b565c878f793db
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-06 19:26:40 -0800

    Call deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -6,9 +6,6 @@ makedocs(
     modules = [MyPlayground]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo="github.com/tkf/MyPlayground.jl",
+)
