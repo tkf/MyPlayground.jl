8edcf196497badd09eb100cb0d946971c4eb09ab
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-26 22:03:11 -0800

    Fix should_push_preview

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(
 function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
     event_path === nothing && return false
     event = JSON.parsefile(event_path)
-    labels = event["pull_request"]["labels"]
+    labels = [x["name"] for x in event["pull_request"]["labels"]]
     # https://developer.github.com/v3/activity/events/types/#pullrequestevent
     yes = "push_preview" in labels
     if yes
