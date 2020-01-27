e17845271c2cf4ad1a65e71af501eba7ce4403a5
Takafumi Arakaki <aka.tkf@gmail.com>
2020-01-26 21:54:44 -0800

    Push preview only when `push_preview` label exists

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter
+using JSON
 using MyPlayground
 
 makedocs(
@@ -7,7 +8,15 @@ makedocs(
     strict = true,
 )
 
+function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
+    event_path === nothing && return false
+    event = JSON.parsefile(event_path)
+    labels = event["pull_request"]["labels"]
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    return "push_preview" in labels
+end
+
 deploydocs(
     repo = "github.com/tkf/MyPlayground.jl",
-    push_preview = true,
+    push_preview = should_push_preview(),
 )
