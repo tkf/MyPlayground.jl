c12b82679aa80705ed2cb9b639e248ae70622959
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-26 19:00:48 -0800

    Run isatty on stdout

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -3,4 +3,6 @@ module MyPlayground
 # Use README as the docstring of the module:
 @doc read(joinpath(dirname(@__DIR__), "README.md"), String) MyPlayground
 
+stdout_isatty() = ccall(:isatty, Cint, (Cint,), Base._fd(stdout))
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using MyPlayground
 using Test
 
+@show MyPlayground.stdout_isatty()
+
 @show Sys.CPU_THREADS
 Sys.cpu_summary()
 
