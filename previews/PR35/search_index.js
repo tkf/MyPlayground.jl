d4a1e9ef5078bf0ac1a11b528c9d0103706742db
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-03 22:23:01 +0000

    build based on 6290ae7

---
+++ previews/PR35/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MyPlayground.jl","page":"MyPlayground.jl","title":"MyPlayground.jl","text":"","category":"section"},{"location":"","page":"MyPlayground.jl","title":"MyPlayground.jl","text":"Documentation for MyPlayground.jl","category":"page"},{"location":"","page":"MyPlayground.jl","title":"MyPlayground.jl","text":"MyPlayground","category":"page"},{"location":"#MyPlayground","page":"MyPlayground.jl","title":"MyPlayground","text":"MyPlayground\n\n(Image: Build Status)\n\n(Image: Coverage Status)\n\n(Image: codecov.io)\n\n\n\n\n\n","category":"module"}]
+}
