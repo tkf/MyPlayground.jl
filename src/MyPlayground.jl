f2a54df0dfe5cbb52c1ecc416dd97406112507a8
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-26 18:10:40 -0700

    Trying reproduce Maybe.jl failure

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -1,6 +1,7 @@
 module MyPlayground
 
-# Use README as the docstring of the module:
-@doc read(joinpath(dirname(@__DIR__), "README.md"), String) MyPlayground
+module Extras end
+
+@doc "Extras docstring" Extras
 
 end # module
