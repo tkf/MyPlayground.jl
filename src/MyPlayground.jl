488f72e191e5c18aadca64b3226ed2fdb201cfd3
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-14 05:25:17 -0400

    Try using reviewdog/action-suggester

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -3,4 +3,9 @@ module MyPlayground
 # Use README as the docstring of the module:
 @doc read(joinpath(dirname(@__DIR__), "README.md"), String) MyPlayground
 
+function  badly(
+formatted
+)
+code end
+
 end # module
