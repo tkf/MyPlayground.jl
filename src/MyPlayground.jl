b5c8d5f4a9927b486c17ffbcc47281ac44bd077e
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-28 13:42:06 +0900

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -3,8 +3,4 @@ module MyPlayground
 # Use README as the docstring of the module:
 @doc read(joinpath(dirname(@__DIR__), "README.md"), String) MyPlayground
 
-badly() =
-styled,
-code
-
 end # module
