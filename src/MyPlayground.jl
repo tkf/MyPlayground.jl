1d4c059e6a8c8aaa4a6c882389ea60b09f674590
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-28 00:32:54 -0400

    Use tkf/julia-code-style-suggesters@d6ca82215a79ec77ccc495b90b502a4936444d05

--- src/MyPlayground.jl
+++ src/MyPlayground.jl
@@ -3,4 +3,8 @@ module MyPlayground
 # Use README as the docstring of the module:
 @doc read(joinpath(dirname(@__DIR__), "README.md"), String) MyPlayground
 
+badly() =
+styled,
+code
+
 end # module
